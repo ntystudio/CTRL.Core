4fe5938a7e2a25de11b0b6f19161260d19d403ea
Tim Oxley <secoif@gmail.com>
2025-02-27 13:26:54 -0500

    Further fix + clean up thumbnail renderer.

--- Source/CTRLCoreEditor/CTRLThumbnailRenderer.cpp
+++ Source/CTRLCoreEditor/CTRLThumbnailRenderer.cpp
@@ -12,6 +12,84 @@
 
 #include "UObject/UObjectIterator.h"
 
+//~ ──────────────────────────── UCTRLThumbnailSubsystem ───────────────────────────── ~//
+
+UCTRLThumbnailSubsystem::ThisClass* UCTRLThumbnailSubsystem::Get()
+{
+	if (!GEditor) return nullptr;
+	return GEditor->GetEditorSubsystem<ThisClass>();
+}
+
+void UCTRLThumbnailSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Collection.InitializeDependency<UContentBrowserDataSubsystem>();
+	Super::Initialize(Collection);
+	FCoreDelegates::OnPostEngineInit.AddUObject(this, &ThisClass::PostEngineInit);
+}
+
+void UCTRLThumbnailSubsystem::RegisterThumbnailRenderers()
+{
+	// debounce registration to next editor tick
+	if (!GEditor) return;
+	if (QueuedUpdateHandle.IsValid()) return;
+	QueuedUpdateHandle = GEditor->GetTimerManager()->SetTimerForNextTick(this, &ThisClass::RegisterThumbnailRenderersInternal);
+}
+
+void UCTRLThumbnailSubsystem::RegisterThumbnailRenderersInternal()
+{
+	QueuedUpdateHandle.Invalidate();
+	if (!GIsEditor) return;
+
+	// Find Classes Implementing UCTRLStaticMeshThumbnailProvider
+	TSet<UClass*> Classes;
+	for (TObjectIterator<UClass> ClassIt; ClassIt; ++ClassIt)
+	{
+		if (ClassIt->ImplementsInterface(UCTRLStaticMeshThumbnailProvider::StaticClass()))
+		{
+			Classes.Add(*ClassIt);
+		}
+	}
+
+	// Register Thumbnail Renderers
+	auto& ThumbnailManager = UThumbnailManager::Get();
+	for (auto const AssetType : Classes)
+	{
+		if (!AssetType) continue;
+		ThumbnailManager.UnregisterCustomRenderer(AssetType); // Don't bother tracking what we've already registered, just reregister everything
+		ThumbnailManager.RegisterCustomRenderer(AssetType, UCTRLStaticMeshThumbnailProviderRenderer::StaticClass());
+	}
+}
+
+void UCTRLThumbnailSubsystem::PostEngineInit()
+{
+	if (!GEditor) return;
+	// If we register too early e.g. PostEngineInit, the thumbnails tend to go blank? The below hooks seem to be reliable though.
+	if (auto const ContentBrowser = GEditor->GetEditorSubsystem<UContentBrowserDataSubsystem>())
+	{
+		ContentBrowser->OnItemDataDiscoveryComplete().AddUObject(this, &ThisClass::RegisterThumbnailRenderers);
+		ContentBrowser->OnItemDataRefreshed().AddUObject(this, &ThisClass::RegisterThumbnailRenderers);
+		ContentBrowser->OnItemDataUpdated().AddUObject(this, &ThisClass::OnItemDataUpdated);
+	}
+}
+
+void UCTRLThumbnailSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+	// we should probably unregister but whatever
+}
+
+void UCTRLThumbnailSubsystem::OnAssetsAdded(TArrayView<FAssetData const> AssetDatas)
+{
+	RegisterThumbnailRenderers();
+}
+
+void UCTRLThumbnailSubsystem::OnItemDataUpdated(TArrayView<FContentBrowserItemDataUpdate const> ContentBrowserItemDataUpdates)
+{
+	RegisterThumbnailRenderers();
+}
+
+//~ ────────────────────── StaticMeshThumbnailProviderRenderer ─────────────────────── ~//
+
 bool UCTRLStaticMeshThumbnailProviderRenderer::CanVisualizeAsset(UObject* Object)
 {
 	if (auto const AsThumbnailProvider = Cast<ICTRLStaticMeshThumbnailProvider>(Object))
@@ -39,79 +117,4 @@ void UCTRLStaticMeshThumbnailProviderRenderer::Draw(
 			Super::Draw(ThumbnailMesh, X, Y, Width, Height, Target, Canvas, bAdditionalViewFamily);
 		}
 	}
-}
-
-UCTRLThumbnailSubsystem::ThisClass* UCTRLThumbnailSubsystem::Get()
-{
-	if (GEditor)
-	{
-		return GEditor->GetEditorSubsystem<ThisClass>();
-	}
-	return nullptr;
-}
-
-void UCTRLThumbnailSubsystem::Initialize(FSubsystemCollectionBase& Collection)
-{
-	Collection.InitializeDependency<UContentBrowserDataSubsystem>();
-	Super::Initialize(Collection);
-	FCoreDelegates::OnPostEngineInit.AddUObject(this, &ThisClass::PostEngineInit);
-}
-
-void UCTRLThumbnailSubsystem::PostEngineInit()
-{
-	if (!GEditor) return;
-	if (auto const ContentBrowser = GEditor->GetEditorSubsystem<UContentBrowserDataSubsystem>())
-	{
-		ContentBrowser->OnItemDataDiscoveryComplete().AddUObject(this, &ThisClass::RegisterThumbnailRenderers);
-		ContentBrowser->OnItemDataRefreshed().AddUObject(this, &ThisClass::RegisterThumbnailRenderers);
-		ContentBrowser->OnItemDataUpdated().AddUObject(this, &ThisClass::OnItemDataUpdated);
-	}
-}
-
-void UCTRLThumbnailSubsystem::RegisterThumbnailRenderers()
-{
-	if (!GEditor) return;
-	if (QueuedUpdateHandle.IsValid()) return;
-	QueuedUpdateHandle = GEditor->GetTimerManager()->SetTimerForNextTick(this, &ThisClass::RegisterThumbnailRenderersInternal);
-}
-
-void UCTRLThumbnailSubsystem::RegisterThumbnailRenderersInternal()
-{
-	QueuedUpdateHandle.Invalidate();
-	if (!GIsEditor) return;
-	TSet<UClass*> Classes;
-	for (TObjectIterator<UClass> ClassIt; ClassIt; ++ClassIt)
-	{
-		if (!ClassIt->ImplementsInterface(UCTRLStaticMeshThumbnailProvider::StaticClass())) continue;
-
-		Classes.Add(*ClassIt);
-	}
-
-	auto& ThumbnailManager = UThumbnailManager::Get();
-	for (auto const AssetType : Classes)
-	{
-		if (!AssetType) continue;
-		ThumbnailManager.UnregisterCustomRenderer(AssetType);
-		ThumbnailManager.RegisterCustomRenderer(AssetType, UCTRLStaticMeshThumbnailProviderRenderer::StaticClass());
-	}
-}
-
-void UCTRLThumbnailSubsystem::Deinitialize()
-{
-	Super::Deinitialize();
-}
-
-void UCTRLThumbnailSubsystem::OnAssetsAdded(TArrayView<FAssetData const> AssetDatas)
-{
-	RegisterThumbnailRenderers();
-}
-
-void UCTRLThumbnailSubsystem::OnInitialScanComplete()
-{
-	RegisterThumbnailRenderers();
-}
-
-void UCTRLThumbnailSubsystem::OnItemDataUpdated(TArrayView<FContentBrowserItemDataUpdate const> ContentBrowserItemDataUpdates)
-{
-	RegisterThumbnailRenderers();
 }
--- Source/CTRLCoreEditor/CTRLThumbnailRenderer.h
+++ Source/CTRLCoreEditor/CTRLThumbnailRenderer.h
@@ -14,6 +14,32 @@
 class UStaticMesh;
 
 /**
+ * Handles registering thumbnail visualizers for ICTRLStaticMeshThumbnailProvider types.
+ * @see https://zomgmoz.tv/unreal/Editor-customization/Custom-asset-editor-thumbnails
+ */
+UCLASS(NotBlueprintType)
+class CTRLCOREEDITOR_API UCTRLThumbnailSubsystem : public UEditorSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	static ThisClass* Get();
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	void RegisterThumbnailRenderers();
+
+protected:
+	FTimerHandle QueuedUpdateHandle;
+
+	void RegisterThumbnailRenderersInternal();
+	void OnAssetsAdded(TArrayView<FAssetData const> AssetDatas);
+	void OnItemDataUpdated(TArrayView<FContentBrowserItemDataUpdate const> ContentBrowserItemDataUpdates);
+	void PostEngineInit();
+};
+
+/**
  * Takes an object that implements ICTRLStaticMeshThumbnailProvider, and uses GetThumbnailMesh() to render a static mesh thumbnail.
  */
 UCLASS()
@@ -25,27 +51,4 @@ public:
 	virtual bool CanVisualizeAsset(UObject* Object) override;
 
 	virtual void Draw(UObject* Object, int32 const X, int32 const Y, uint32 const Width, uint32 const Height, FRenderTarget* Target, FCanvas* Canvas, bool const bAdditionalViewFamily) override;
-};
-
-/**
- * Handles registering thumbnail visualizers for ICTRLStaticMeshThumbnailProvider types.
- * @see https://zomgmoz.tv/unreal/Editor-customization/Custom-asset-editor-thumbnails
- */
-UCLASS(NotBlueprintType)
-class CTRLCOREEDITOR_API UCTRLThumbnailSubsystem : public UEditorSubsystem
-{
-	GENERATED_BODY()
-
-public:
-	static ThisClass* Get();
-	void RegisterThumbnailRenderers();
-	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-	virtual void Deinitialize() override;
-
-protected:
-	void RegisterThumbnailRenderersInternal();
-	void OnAssetsAdded(TArrayView<FAssetData const> AssetDatas);
-	void OnInitialScanComplete();
-	void OnItemDataUpdated(TArrayView<FContentBrowserItemDataUpdate const> ContentBrowserItemDataUpdates);
-	void PostEngineInit();
 };
